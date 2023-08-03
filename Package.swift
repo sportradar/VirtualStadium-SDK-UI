0f8d83d3cebeb86266571e532b01b94019b6fa75
Gitlab CI Network <network@sportradar.com>
2023-08-03 13:36:05 +0200

    VirtualStadiumUISDK 0.1 release

--- Package.swift
+++ Package.swift
@@ -2,23 +2,33 @@
 // swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
-
 let package = Package(
-    name: "VirtualStadiumUISDK",
-    platforms: [.iOS(.v13)],
+    name:  "VirtualStadiumUISDK",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "VirtualStadiumUISDK",
-            targets: ["VirtualStadiumUISDK"]
+            targets: [VirtualStadiumUISDKTarget]
         ),
+    ],
+    dependencies: [
+        .package(url: git@gitlab.sportradar.ag:mdp-next/virtual-stadium-ios-spm.git, exact: ),
     ],
     targets: [
         .binaryTarget(
             name: "VirtualStadiumUISDK",
-            url: "https://vs-artifacts.mapinonprod.sportradar.com/apple/virtual-stadium-ios-ui-sdk/d9ef07c6/release/VirtualStadiumUISDK.xcframework.zip",
-            checksum: "5616d4be305a83b7492d975f5ea10c026278ada4da517c3f56f232d8f1863944"
-        )
+            url: "https://vs-artifacts.mapinonprod.sportradar.com/apple/virtual-stadium-ios-ui-sdk/2328f5e8/release/VirtualStadiumUISDK.xcframework.zip",
+            checksum: "d0e625af2105506e80635aa3b9e4c1866c6bfc26f041c2450b3fc137326d7e7f"
+        ),
+        .target(
+            name: VirtualStadiumUISDKTarget,
+            dependencies: [
+                .target(name: "VirtualStadiumUISDK"),
+                .product(name: VirtualStadiumDataSDK, package: virtual-stadium-ios-spm),
+            ],
+            path: VirtualStadiumUISDKTarget)
     ]
 )
 
