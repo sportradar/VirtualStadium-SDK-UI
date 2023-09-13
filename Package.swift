f0412e3deb8c189f4c107f0e416be1e0d38ced7a
Gitlab CI Network <network@sportradar.com>
2023-09-13 14:41:20 +0200

    VirtualStadiumUISDK 1.0 release

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VirtualStadiumUISDK",
-            url: "https://vs-artifacts.mapinonprod.sportradar.com/apple/virtual-stadium-ios-ui-sdk/68c1afa5/release/VirtualStadiumUISDK.xcframework.zip",
-            checksum: ""
+            url: "https://vs-artifacts.mapinonprod.sportradar.com/apple/virtual-stadium-ios-ui-sdk/61aafc1b/release/VirtualStadiumUISDK.xcframework.zip",
+            checksum: "4254749667c1d8d464eb624e57e81ee44dbb0f90d7097321049e4c4158b607f6"
         ),
         .target(
             name: "VirtualStadiumUISDKTarget",
